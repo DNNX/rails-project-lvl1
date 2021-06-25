4b938de3ed706fb1009c261d74a89a05e2d6d8b5
Viktar Basharymau <viktar.basharymau@gmail.com>
2021-06-25 18:09:40 +0200

    Initial version of form builder

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -3,7 +3,14 @@
 require 'hexlet_code/version'
 require 'hexlet_code/tag'
 
+# Module for generating forms.
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.form_for(_model, url: '#')
+    Tag.build('form', action: url, method: 'post') do
+      # TODO: use `model` here
+      ''
+    end
+  end
 end
--- spec/hexlet_code_spec.rb
+++ spec/hexlet_code_spec.rb
@@ -1,7 +1,22 @@
 # frozen_string_literal: true
 
+require 'hexlet_code'
+
 RSpec.describe HexletCode do
+  let(:user_class) { Struct.new(:name, :job, keyword_init: true) }
+  let(:user) { user_class.new(name: 'rob') }
+
   it 'has a version number' do
     expect(HexletCode::VERSION).not_to be nil
+  end
+
+  it 'generates forms with default action' do
+    expect(described_class.form_for(user))
+      .to eq('<form action="#" method="post"></form>')
+  end
+
+  it 'generates forms with given url' do
+    expect(described_class.form_for(user, url: '/users'))
+      .to eq('<form action="/users" method="post"></form>')
   end
 end
